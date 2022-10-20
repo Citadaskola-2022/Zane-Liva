c81029e2e0e2ddd0407acfcfd8d8bb60705d52e4
Zane Liva <zaneliva@Zanes-MacBook-Air.local>
2022-10-20 15:14:14 +0300

    phone

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use App\Phone;
+
 spl_autoload_register(function ($class) {
     $class = __DIR__ .'/../'. lcfirst(str_replace('\\', '/', $class)) . '.php';
     require_once $class;
@@ -9,8 +11,17 @@ spl_autoload_register(function ($class) {
 
 });
 
-$subBill = new \App\Invoices\Sub\Bill();
 
-var_dump($subBill);
+$iphone = new Phone(11, 'Apple', 144, 71.4);
+$android = new Phone(22, 'Samsung');
+
+
+$iphone->installApp('Facebook');
+$iphone->installApp('Instagram');
+$iphone->installApp('Duolingo');
+
+echo implode( '',$iphone->getInstalledApps());
+
+$iphone->turnOnSetting("DND");
 
 //require __DIR__ . '/../bootstrap/app.php';
